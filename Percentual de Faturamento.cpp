eb21d7cda0c3a3e33f859ca53207a1ba92f0a0b7
GustaHSR <126812739+GustaHSR@users.noreply.github.com>
2023-03-06 12:51:45 -0300

    Update Percentual de Faturamento.cpp

--- Percentual de Faturamento.cpp
+++ Percentual de Faturamento.cpp
@@ -6,6 +6,19 @@ typedef long long ll;
 typedef long double ld;
 #define pb push_back
 
+template <typename K, typename V>
+multimap<V, K>
+
+invertMap(map<K,V> const& myMap) //Função que inverte o map da porcentagem dos estados para imprimir as porcentagens em ordem crescente
+{
+    multimap<V,K>porcentagem_ordenada;
+
+    for (auto const& pair : myMap) {
+        porcentagem_ordenada.insert(make_pair(pair.second,pair.first));
+    }
+    return porcentagem_ordenada;
+}
+
 int main () {
 
     string estado; // string para guardar os estados
@@ -13,7 +26,6 @@ int main () {
     ld total = 0; //variavel que guarda a soma dos faturamentos
     
     map<string,ld>porcentagem; //map que indexa os estados com os faturamentos
-    map<string,ld>:: iterator it;
 
     cout << "Digite o estado e depois o valor do faturamento mensal da distribuidora daquele estado:" << endl << endl;
     cout << "Estado: ";
@@ -31,12 +43,14 @@ int main () {
         cout << "Estado: ";
         cin.ignore();
     }
-    for (it = porcentagem.begin(); it != porcentagem.end(); it++) { //substitui os faturamentos pelos respectivos percentuais
-        it->second = (it->second*100)/total;
+    for (auto it = porcentagem.begin(); it != porcentagem.end();it++) { //substitui os faturamentos pelos respectivos percentuais
+        it->second = it->second *100 /total;
     }
     cout << endl << "Segue estados com seus respectivos percentuais de faturamento da distribuidora:" << endl << endl;
-    for (it = porcentagem.begin(); it != porcentagem.end();it++) { // Imprime os percentuais de cada estado
-        cout << "{ " << it->first << " --> " << fixed << setprecision(2) << it->second << "% }" << endl;
+
+    multimap<ld,string>porcentagem_ordenada = invertMap(porcentagem); //Multimap para ordenar o map de porcentagens em ordem crescente
+
+    for (auto it = porcentagem_ordenada.begin(); it != porcentagem_ordenada.end(); it++) { // Imprime os percentuais de cada estado
+        cout << "{ " << it->second << " --> " << it->first << "% }" << endl;
     }
-    
 }
